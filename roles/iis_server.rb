00d5d5ce03a59b95b93f258719e96c5ee2ee0109
Mat Schaffer <mat@schaffer.me>
2011-11-29 09:18:31 -0500

    Don't include chef client in IIS role

--- roles/iis_server.rb
+++ roles/iis_server.rb
@@ -1,4 +1,4 @@
 name "iis_server"
 description "Sets up an IIS server"
-run_list ["recipe[iis]", "recipe[windows_extras::chef_client]"]
-default_attributes chef_client: { interval: 10 }, iis: { accept_eula: true }
+run_list ["recipe[iis]"]
+default_attributes iis: { accept_eula: true }
