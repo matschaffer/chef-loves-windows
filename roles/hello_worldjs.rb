f1cd0dc564f9474f0671967e3432cf87a2f99336
Mat Schaffer <mat@schaffer.me>
2011-11-30 18:04:17 -0500

    Include chef-client in hello world demo

--- roles/hello_worldjs.rb
+++ roles/hello_worldjs.rb
@@ -1,3 +1,4 @@
 name "hello_worldjs"
 description "Sets up a hello world Node.js app"
-run_list ["recipe[nodejs]", "recipe[hello_worldjs]"]
+run_list ["recipe[windows_extras::chef_client]", "recipe[nodejs]", "recipe[hello_worldjs]"]
+default_attributes chef_client: { interval: 10 }
